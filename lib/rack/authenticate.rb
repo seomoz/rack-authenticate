b6502c17bad5bf3b744ce5c1997a35a8d0331c36
Myron Marston <myron.marston@gmail.com>
2011-12-14 15:22:33 -0800

    Add method to generate new secret key.

--- lib/rack/authenticate.rb
+++ lib/rack/authenticate.rb
@@ -1,6 +1,11 @@
 module Rack
   module Authenticate
-    # Your code goes here...
+    def self.new_secret_key
+      require 'base64'
+      require 'securerandom'
+      require 'digest/sha2'
+      Base64.encode64(Digest::SHA2.new(512).digest(SecureRandom.random_bytes(512))).chomp
+    end
   end
 end
 
---
+++ spec/rack/authenticate_spec.rb
@@ -0,0 +1,17 @@
+require 'rack/authenticate'
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.filter_run :f
+  c.run_all_when_everything_filtered = true
+end
+
+module Rack
+  describe Authenticate do
+    describe "#new_secret_key" do
+      it "generates a long random string" do
+        Rack::Authenticate.new_secret_key.should match(/[A-Za-z0-9\\\/]{60,}/)
+      end
+    end
+  end
+end
